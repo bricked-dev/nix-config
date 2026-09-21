e7aec9ec22ce04b7890e69b4118bcd2f52b3981e
Bricked <rocket@bricked.dev>
2026-09-21 21:34:30 +0200

    rust: add web server for docs

--- modules/rust/nixos.nix
+++ modules/rust/nixos.nix
@@ -3,13 +3,38 @@
   lib,
   ...
 }: let
-  inherit (lib) mkIf optional;
+  inherit (lib) mkEnableOption mkIf mkOption optional types;
 
   cfg = config.programs.rust;
 in {
   imports = [./common.nix];
 
+  options.programs.rust = {
+    docs.server = {
+      enable =
+        mkEnableOption "local documentation server"
+        // {
+          default = true;
+        };
+
+      hostName = mkOption {
+        description = "Hostname for the documentation server.";
+        type = types.str;
+        default = "rust.docs.localhost";
+      };
+    };
+  };
+
   config = mkIf cfg.enable {
     environment.systemPackages = [cfg.package] ++ optional cfg.docs.enable cfg.docs.package;
+
+    services.caddy = mkIf cfg.docs.server.enable {
+      enable = true;
+
+      virtualHosts.${cfg.docs.server.hostName}.extraConfig = ''
+        root ${config.programs.rust.package}/share/doc/rust/html
+        file_server
+      '';
+    };
   };
 }
